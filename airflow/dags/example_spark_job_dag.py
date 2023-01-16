cf8a9e02157b0d7b95970f19d3a1244140d998c6
jered.little <jeredlittle1996@gmail.com>
2023-01-16 05:41:22 -0700

    add example spark job

---
+++ airflow/dags/example_spark_job_dag.py
@@ -0,0 +1,15 @@
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
+from airflow import DAG
+from datetime import datetime
+import os
+
+with DAG(dag_id="example_spark_job",
+         start_date=datetime(2021,1,1),
+         schedule_interval="@hourly",
+         catchup=False) as dag:
+    spark_task = SparkKubernetesOperator(
+        task_id = "example_spark_job",
+        application_file = "./application.yaml",
+        namespace = "compute-plane",
+        #kubernetes_conn_id='kubernetes_default',
+    )
